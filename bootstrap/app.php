4d3c90b2d5e7658ae69b7114a5004e7a781314ff
rizkychi <rizkynhae@gmail.com>
2025-08-25 20:10:46 +0700

    fix: revert trust proxies middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -24,13 +24,12 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         // Trust all proxies
-        $middleware->trustProxies(at: '*', headers: Request::HEADER_X_FORWARDED_FOR |
-            Request::HEADER_X_FORWARDED_HOST |
-            Request::HEADER_X_FORWARDED_PORT |
-            Request::HEADER_X_FORWARDED_PROTO |
-            Request::HEADER_X_FORWARDED_AWS_ELB
-        );
-
+        // $middleware->trustProxies(at: '*', headers: Request::HEADER_X_FORWARDED_FOR |
+        //     Request::HEADER_X_FORWARDED_HOST |
+        //     Request::HEADER_X_FORWARDED_PORT |
+        //     Request::HEADER_X_FORWARDED_PROTO |
+        //     Request::HEADER_X_FORWARDED_AWS_ELB
+        // );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
